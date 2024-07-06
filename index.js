b00cd82a91e8990ebdcfdf6e97ed2b57d4b52f6c
azmi adan <azmi.adan2032@gmail.com>
2024-07-06 08:46:41 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    messages.push(message);
+  }
+  return messages;
+}
+const names = ["Guadalupe", "Ollie", "Aki"];
+const eventName = "surprise";
+const result = writeCards(names, eventName);
+console.log(result)
+let spy;
+
+beforeEach(() => {
+  spy = sinon.spy(console, 'log');
+});
+
+function countDown(n) {
+  let i = n;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
